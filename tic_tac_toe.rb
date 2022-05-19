f6491accdd0206ab6cace5b1c3ef1fa7cbc03c8b
tombodiddly <tombodiddly@gmail.com>
2022-05-19 15:18:59 -0700

    Add playable command line contols

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,36 +2,81 @@
 
 # Game Class
 class Game
-  attr_reader :player1, :player2, :board
+  attr_reader :player1, :player2, :board, :lines, :total_moves
 
   def initialize
     @player1 = Player.new('Player 1', 'X')
     @player2 = Player.new('Player 2', 'O')
     @board = Board.new
-  end
-
-  def move(player)
-    puts "#{player.name}'s turn... Choose an open slot."
-    move = gets.chomp
-    board.update_grid(move.to_i, player.symbol)
+    @total_moves = 0
   end
 
   def current_player
-    board.total_moves.zero? || (board.total_moves % 2).zero? ? player1 : player2
+    total_moves.zero? || (total_moves % 2).zero? ? player1 : player2
+  end
+
+  def move(player)
+    puts "\n#{player.name}'s turn... Choose an open slot Bro."
+    move = gets.chomp
+    update_grid(move.to_i, player.symbol)
+  end
+
+  def update_grid(slot, symbol)
+    if board.grid[slot] == slot.to_s
+      board.grid[slot] = symbol
+      puts "\n#{current_player.name} marked slot ##{slot}: #{symbol}"
+      @total_moves += 1
+    else
+      puts "Slot ##{slot} is already taken Bro. \nPlease choose another slot..."
+    end
+  end
+
+  def win_lines(grid)
+    @lines = [
+      [grid[1], grid[2], grid[3]], [grid[4], grid[5], grid[6]],
+      [grid[7], grid[8], grid[9]], [grid[1], grid[4], grid[7]],
+      [grid[2], grid[5], grid[8]], [grid[3], grid[6], grid[9]],
+      [grid[1], grid[5], grid[9]], [grid[3], grid[5], grid[7]]
+      ]
+  end
+
+  def check_win
+    win_lines(board.grid)
+    lines.select do |line|
+      if (line.uniq.count == 1) == true
+        @total_moves += 1
+        puts "\nGame Over Bro! #{line[0]} wins!!!\nCongratulations #{current_player.name}!!!"
+        play_again
+      end
+    end
+  end
+
+  def check_draw
+    draw = board.grid.values.map(&:to_i)
+    return unless draw.all?(0)
+
+    puts "\nGame Over Bro! It's a draw...\n"
+    play_again
+  end
+
+  def board_refresh
+    @board = Board.new
+    @total_moves = 0
+    board.show_grid
   end
 
   def play_again
-    puts "Wanna play another round?\nEnter 'y' or 'n'..."
-    answer = gets.chomp.downcase
-    until answer == 'y' || answer == 'n'
+    puts "\nWanna play another round Bro?\nEnter 'y' or 'n'..."
+    answer = [gets.chomp.downcase]
+    until answer.include?('y') || answer.include?('n')
       puts "Enter a valid answer... 'y' or 'n'..."
-      answer = gets.chomp.downcase
+      answer = [gets.chomp.downcase]
     end
-    answer == 'y' ? @board = Board.new : quit_game
+    answer.include?('y') ? board_refresh : quit_game
   end
 
   def quit_game
-    puts "\nThanks for playing! See you next time!\n..."
+    puts "\nThanks for playing! See you next time Bro!\n..."
     sleep(1)
     exit(0)
   end
@@ -39,7 +84,7 @@ end
 
 # Board Class
 class Board
-  attr_reader :grid, :total_moves
+  attr_reader :grid
 
   def initialize
     @grid = {
@@ -47,49 +92,12 @@ class Board
       4 => '4', 5 => '5', 6 => '6',
       7 => '7', 8 => '8', 9 => '9'
     }
-    @total_moves = 0
-    puts "Let's play... \n \n"
-    sleep(1)
-    show_grid
   end
 
   def show_grid
-    puts "  #{grid[1]} * #{grid[2]} * #{grid[3]} \n
+    puts "\n  #{grid[1]} * #{grid[2]} * #{grid[3]} \n
   #{grid[4]} * #{grid[5]} * #{grid[6]} \n
   #{grid[7]} * #{grid[8]} * #{grid[9]}"
-  end
-
-  def update_grid(slot, symbol)
-    if grid[slot] == slot.to_s
-      grid[slot] = symbol
-      @total_moves += 1
-      show_grid
-      check_win
-      check_draw
-    else
-      puts "Slot ##{slot} is already taken. \nPlease choose another slot..."
-    end
-  end
-
-  def check_win
-    lines = [
-      [grid[1], grid[2], grid[3]], [grid[4], grid[5], grid[6]],
-      [grid[7], grid[8], grid[9]], [grid[1], grid[4], grid[7]],
-      [grid[2], grid[5], grid[8]], [grid[3], grid[6], grid[9]],
-      [grid[1], grid[5], grid[9]], [grid[3], grid[5], grid[7]]
-      ]
-    lines.select do |line|
-      if (line.uniq.count == 1) == true
-        puts " \n Game Over! #{line[0]} wins!!!\n"
-      end
-    end
-  end
-
-  def check_draw
-    draw = grid.values.map {|val| val.to_i}
-    if draw.all?(0)
-      puts " \n Game Over! It's a draw...\n"
-    end
   end
 end
 
@@ -101,4 +109,17 @@ class Player
     @name = name
     @symbol = symbol
   end
+end
+
+# Game Play
+puts "\nLet's play TIC - TAC - BRO!!!"
+sleep(1)
+game = Game.new
+game.board.show_grid
+
+while game.total_moves < 9
+  game.move(game.current_player)
+  game.board.show_grid
+  game.check_win
+  game.check_draw
 end
